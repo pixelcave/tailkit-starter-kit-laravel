9b6b42fdc008f7f689f8d948fe3a8744ae9d596c
John Champ <72492771+john-champ@users.noreply.github.com>
2023-11-02 12:01:37 +0200

    Prettier support

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,169 +1,171 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Tailkit Project</title>
 
-  <title>Tailkit Project</title>
+    <!-- Favicons -->
+    <link rel="icon" href="favicon.svg" sizes="any" type="image/svg+xml" />
+    <link rel="icon" href="favicon.png" type="image/png" />
 
-  <!-- Favicons -->
-  <link rel="icon" href="favicon.svg" sizes="any" type="image/svg+xml">
-  <link rel="icon" href="favicon.png" type="image/png">
-  
-  <!-- Inter web font from Bunny.net (GDPR compliant) -->
-  <link rel="preconnect" href="https://fonts.bunny.net">
-  <link href="https://fonts.bunny.net/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Inter web font from Bunny.net (GDPR compliant) -->
+    <link rel="preconnect" href="https://fonts.bunny.net" />
+    <link
+      href="https://fonts.bunny.net/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap"
+      rel="stylesheet"
+    />
 
-  @vite(['resources/css/tailkit.css', 'resources/js/app.js'])
-</head>
-  
-<body>
-  <!--
-  
-  ADD YOUR MAIN CONTENT BELOW
-        
-  -->
+    @vite(["resources/css/tailkit.css", "resources/js/app.js"])
+  </head>
 
-  <div class="min-h-screen bg-white dark:bg-gray-900 dark:text-gray-100">
-    <div
-      class="container mx-auto space-y-12 px-4 py-12 text-center sm:py-20 xl:max-w-6xl"
-    >
-      <div>
-        <a
-          href="/"
-          class="active group inline-flex items-center justify-center gap-2 active:opacity-75"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 256 256"
-            class="inline-block h-8 w-8 transition duration-150 ease-out group-hover:-rotate-12 group-active:rotate-0"
+  <body>
+    <!--
+
+    ADD YOUR MAIN CONTENT BELOW
+
+    -->
+
+    <div class="min-h-screen bg-white dark:bg-gray-900 dark:text-gray-100">
+      <div
+        class="container mx-auto space-y-12 px-4 py-12 text-center sm:py-20 xl:max-w-6xl"
+      >
+        <div>
+          <a
+            href="/"
+            class="active group inline-flex items-center justify-center gap-2 active:opacity-75"
           >
-            <circle
-              cx="128"
-              cy="128"
-              r="120"
-              fill-opacity="0"
-              stroke="#13cdc6"
-              stroke-linejoin="round"
-              stroke-width="16"
-              fill="#293340"
-            ></circle>
-            <circle cx="128" cy="128" r="96" fill="#293340"></circle>
-            <path
-              d="M130.424 165.032a20 20 0 01-14.142 24.495l-8.842 2.373a20 20 0 01-24.495-14.142l-2.369-8.842a20 20 0 0114.142-24.5l8.842-2.369a20 20 0 0124.5 14.142z"
-              fill="#818cf8"
-              fill-rule="evenodd"
-            ></path>
-            <path
-              d="M191.9 148.561a20 20 0 01-14.143 24.5l-8.842 2.369a20 20 0 01-24.494-14.142l-2.37-8.842a20 20 0 0114.142-24.5l8.842-2.369a20 20 0 0124.5 14.142z"
-              fill="#f472b6"
-              fill-rule="evenodd"
-            ></path>
-            <path
-              d="M113.953 103.561a20 20 0 01-14.142 24.5l-8.842 2.369a20 20 0 01-24.495-14.142l-2.369-8.842a20 20 0 0114.142-24.5l8.842-2.369a20 20 0 0124.495 14.142z"
-              fill="#34d399"
-              fill-rule="evenodd"
-            ></path>
-            <path
-              d="M175.424 87.09a20 20 0 01-14.142 24.495l-8.842 2.369a20 20 0 01-24.5-14.142l-2.369-8.842a20 20 0 0114.142-24.495l8.842-2.369a20 20 0 0124.5 14.142z"
-              fill="#fbbf24"
-              fill-rule="evenodd"
-            ></path>
-          </svg>
-          <span
-            class="text-3xl font-bold transition-opacity duration-150 ease-out group-hover:opacity-75"
-            >Tailkit</span
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 256"
+              class="inline-block h-8 w-8 transition duration-150 ease-out group-hover:-rotate-12 group-active:rotate-0"
+            >
+              <circle
+                cx="128"
+                cy="128"
+                r="120"
+                fill-opacity="0"
+                stroke="#13cdc6"
+                stroke-linejoin="round"
+                stroke-width="16"
+                fill="#293340"
+              ></circle>
+              <circle cx="128" cy="128" r="96" fill="#293340"></circle>
+              <path
+                d="M130.424 165.032a20 20 0 01-14.142 24.495l-8.842 2.373a20 20 0 01-24.495-14.142l-2.369-8.842a20 20 0 0114.142-24.5l8.842-2.369a20 20 0 0124.5 14.142z"
+                fill="#818cf8"
+                fill-rule="evenodd"
+              ></path>
+              <path
+                d="M191.9 148.561a20 20 0 01-14.143 24.5l-8.842 2.369a20 20 0 01-24.494-14.142l-2.37-8.842a20 20 0 0114.142-24.5l8.842-2.369a20 20 0 0124.5 14.142z"
+                fill="#f472b6"
+                fill-rule="evenodd"
+              ></path>
+              <path
+                d="M113.953 103.561a20 20 0 01-14.142 24.5l-8.842 2.369a20 20 0 01-24.495-14.142l-2.369-8.842a20 20 0 0114.142-24.5l8.842-2.369a20 20 0 0124.495 14.142z"
+                fill="#34d399"
+                fill-rule="evenodd"
+              ></path>
+              <path
+                d="M175.424 87.09a20 20 0 01-14.142 24.495l-8.842 2.369a20 20 0 01-24.5-14.142l-2.369-8.842a20 20 0 0114.142-24.495l8.842-2.369a20 20 0 0124.5 14.142z"
+                fill="#fbbf24"
+                fill-rule="evenodd"
+              ></path>
+            </svg>
+            <span
+              class="text-3xl font-bold transition-opacity duration-150 ease-out group-hover:opacity-75"
+            >
+              Tailkit
+            </span>
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://pixelcave.com"
+            class="group mb-2 inline-flex items-center space-x-1 rounded border bg-gray-200/50 px-1.5 py-1 text-sm font-semibold leading-4 text-gray-700 transition hover:border-rose-100 hover:bg-rose-100 hover:text-rose-700 active:border-gray-200 active:bg-gray-200/50 active:text-gray-700 dark:border-gray-700 dark:bg-transparent dark:text-gray-300 dark:hover:border-rose-900 dark:hover:bg-rose-800 dark:hover:text-rose-100 dark:active:border-gray-700 dark:active:bg-gray-700/50 dark:active:text-gray-300"
           >
-        </a>
-      </div>
-      <div>
-        <a
-          href="https://pixelcave.com"
-          class="group mb-2 inline-flex items-center space-x-1 rounded border bg-gray-200/50 px-1.5 py-1 text-sm font-semibold leading-4 text-gray-700 transition hover:border-rose-100 hover:bg-rose-100 hover:text-rose-700 active:border-gray-200 active:bg-gray-200/50 active:text-gray-700 dark:border-gray-700 dark:bg-transparent dark:text-gray-300 dark:hover:border-rose-900 dark:hover:bg-rose-800 dark:hover:text-rose-100 dark:active:border-gray-700 dark:active:bg-gray-700/50 dark:active:text-gray-300"
-        >
-          <span>Crafted with</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-            class="inline-block h-4 w-4 text-rose-500 transition will-change-auto group-hover:animate-ping group-active:animate-none"
+            <span>Crafted with</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+              class="inline-block h-4 w-4 text-rose-500 transition will-change-auto group-hover:animate-ping group-active:animate-none"
+            >
+              <path
+                d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z"
+              ></path>
+            </svg>
+            <span>by pixelcave</span>
+          </a>
+          <h1
+            class="mb-3 text-3xl font-black text-black dark:text-white md:text-4xl lg:mx-auto lg:w-2/3 lg:leading-tight"
           >
-            <path
-              d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z"
-            ></path>
-          </svg>
-          <span>by pixelcave</span>
-        </a>
-        <h1
-          class="mb-3 text-3xl font-black text-black dark:text-white md:text-4xl lg:mx-auto lg:w-2/3 lg:leading-tight"
-        >
-          Build amazing web apps and websites that your users will love using
-        </h1>
-        <h2
-          class="text-lg font-medium leading-relaxed text-gray-700 dark:text-gray-300 lg:mx-auto lg:w-3/5"
-        >
-          Carefully crafted, easy to customize, fully responsive UI
-          Components, Templates and Tools for your Tailwind CSS based
-          projects.
-        </h2>
-      </div>
-      <div class="flex flex-wrap justify-center gap-2 sm:items-center">
-        <a
-          href="https://tailkit.com/login"
-          target="_blank"
-          class="group inline-flex w-full items-center justify-center gap-2 rounded-lg border border-blue-700 bg-blue-700 p-3 text-sm font-semibold leading-5 text-white transition hover:border-blue-800 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 active:border-blue-700 active:bg-blue-700 sm:w-auto sm:px-4"
-        >
-          <svg
-            class="hi-mini hi-rocket-launch inline-block h-5 w-5 opacity-50"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
+            Build amazing web apps and websites that your users will love using
+          </h1>
+          <h2
+            class="text-lg font-medium leading-relaxed text-gray-700 dark:text-gray-300 lg:mx-auto lg:w-3/5"
           >
-            <path
-              fill-rule="evenodd"
-              d="M4.606 12.97a.75.75 0 01-.134 1.051 2.494 2.494 0 00-.93 2.437 2.494 2.494 0 002.437-.93.75.75 0 111.186.918 3.995 3.995 0 01-4.482 1.332.75.75 0 01-.461-.461 3.994 3.994 0 011.332-4.482.75.75 0 011.052.134z"
-              clip-rule="evenodd"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M5.752 12A13.07 13.07 0 008 14.248v4.002c0 .414.336.75.75.75a5 5 0 004.797-6.414 12.984 12.984 0 005.45-10.848.75.75 0 00-.735-.735 12.984 12.984 0 00-10.849 5.45A5 5 0 001 11.25c.001.414.337.75.751.75h4.002zM13 9a2 2 0 100-4 2 2 0 000 4z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          <span>Get Started</span>
-        </a>
-        <a
-          href="https://tailkit.com/docs/documentation"
-          target="_blank"
-          class="group inline-flex w-full items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white p-3 text-sm font-semibold leading-5 text-gray-800 transition hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-300/25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-transparent dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600/50 dark:active:border-gray-700 sm:w-auto sm:px-4"
-        >
-          <svg
-            class="hi-mini hi-document-text inline-block h-5 w-5 opacity-50"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
+            Carefully crafted, easy to customize, fully responsive UI
+            Components, Templates and Tools for your Tailwind CSS based
+            projects.
+          </h2>
+        </div>
+        <div class="flex flex-wrap justify-center gap-2 sm:items-center">
+          <a
+            href="https://tailkit.com/login"
+            target="_blank"
+            class="group inline-flex w-full items-center justify-center gap-2 rounded-lg border border-blue-700 bg-blue-700 p-3 text-sm font-semibold leading-5 text-white transition hover:border-blue-800 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 active:border-blue-700 active:bg-blue-700 sm:w-auto sm:px-4"
           >
-            <path
-              fill-rule="evenodd"
-              d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm2.25 8.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 3a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          <span>Documentation</span>
-        </a>
+            <svg
+              class="hi-mini hi-rocket-launch inline-block h-5 w-5 opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.606 12.97a.75.75 0 01-.134 1.051 2.494 2.494 0 00-.93 2.437 2.494 2.494 0 002.437-.93.75.75 0 111.186.918 3.995 3.995 0 01-4.482 1.332.75.75 0 01-.461-.461 3.994 3.994 0 011.332-4.482.75.75 0 011.052.134z"
+                clip-rule="evenodd"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M5.752 12A13.07 13.07 0 008 14.248v4.002c0 .414.336.75.75.75a5 5 0 004.797-6.414 12.984 12.984 0 005.45-10.848.75.75 0 00-.735-.735 12.984 12.984 0 00-10.849 5.45A5 5 0 001 11.25c.001.414.337.75.751.75h4.002zM13 9a2 2 0 100-4 2 2 0 000 4z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <span>Get Started</span>
+          </a>
+          <a
+            href="https://tailkit.com/docs/documentation"
+            target="_blank"
+            class="group inline-flex w-full items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white p-3 text-sm font-semibold leading-5 text-gray-800 transition hover:border-gray-300 hover:text-gray-900 hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-300/25 active:border-gray-200 active:shadow-none dark:border-gray-700 dark:bg-transparent dark:text-gray-300 dark:hover:border-gray-600 dark:hover:text-gray-200 dark:focus:ring-gray-600/50 dark:active:border-gray-700 sm:w-auto sm:px-4"
+          >
+            <svg
+              class="hi-mini hi-document-text inline-block h-5 w-5 opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm2.25 8.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 3a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <span>Documentation</span>
+          </a>
+        </div>
       </div>
     </div>
-  </div>
 
-  <!--
+    <!--
 
-  ADD YOUR MAIN CONTENT ABOVE
-      
-  -->
-</body>
+    ADD YOUR MAIN CONTENT ABOVE
 
+    -->
+  </body>
 </html>
