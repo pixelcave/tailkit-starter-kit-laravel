1da41a40915cfe82d2fceaa919010c736a73e1a5
John Champ <72492771+john-champ@users.noreply.github.com>
2024-03-01 12:09:37 +0200

    Tailkit 3.15 config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,51 @@
 // Tailkit (Tailwind CSS v3 Configuration)
-import plugin from 'tailwindcss/plugin';
-import defaultTheme from 'tailwindcss/defaultTheme';
-import colors from 'tailwindcss/colors';
+import plugin from "tailwindcss/plugin";
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
 
-import aspectRatio from '@tailwindcss/aspect-ratio';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import aspectRatio from "@tailwindcss/aspect-ratio";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/views/**/*.blade.php"
-  ],
-  darkMode: 'class',
+  content: ["./resources/views/**/*.blade.php"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          'Inter',
-          ...defaultTheme.fontFamily.sans,
-        ]
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
       maxWidth: {
-        '8xl': '90rem',
-        '9xl': '105rem',
-        '10xl': '120rem',
+        "8xl": "90rem",
+        "9xl": "105rem",
+        "10xl": "120rem",
       },
       zIndex: {
-        '1': 1,
-        '60': 60,
-        '70': 70,
-        '80': 80,
-        '90': 90,
-        '100': 100,
+        1: 1,
+        60: 60,
+        70: 70,
+        80: 80,
+        90: 90,
+        100: 100,
+      },
+      keyframes: {
+        "spin-slow": {
+          "100%": {
+            transform: "rotate(-360deg)",
+          },
+        },
+      },
+      animation: {
+        "spin-slow": "spin-slow 8s linear infinite",
       },
       typography: {
         DEFAULT: {
           css: {
             a: {
-              textDecoration: 'none',
-              '&:hover': {
-                opacity: '.75',
+              textDecoration: "none",
+              "&:hover": {
+                opacity: ".75",
               },
             },
             img: {
@@ -55,25 +60,27 @@ export default {
     aspectRatio,
     forms,
     typography,
-    plugin(function({ addUtilities }) {
+    plugin(function ({ addUtilities }) {
       const utilFormSwitch = {
-        '.form-switch': {
-          'border': 'transparent',
-          'background-color': colors.gray[300],
-          'background-image': "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e\")",
-          'background-position': 'left center',
-          'background-repeat': 'no-repeat',
-          'background-size': 'contain !important',
-          'vertical-align': 'top',
-          '&:checked': {
-            'border': 'transparent',
-            'background-color': 'currentColor',
-            'background-image': "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e\")",
-            'background-position': 'right center',
+        ".form-switch": {
+          border: "transparent",
+          "background-color": colors.gray[300],
+          "background-image":
+            "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e\")",
+          "background-position": "left center",
+          "background-repeat": "no-repeat",
+          "background-size": "contain !important",
+          "vertical-align": "top",
+          "&:checked": {
+            border: "transparent",
+            "background-color": "currentColor",
+            "background-image":
+              "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e\")",
+            "background-position": "right center",
           },
-          '&:disabled, &:disabled + label': {
-            'opacity': '.5',
-            'cursor': 'not-allowed',
+          "&:disabled, &:disabled + label": {
+            opacity: ".5",
+            cursor: "not-allowed",
           },
         },
       };
@@ -81,4 +88,4 @@ export default {
       addUtilities(utilFormSwitch);
     }),
   ],
-}
+};
