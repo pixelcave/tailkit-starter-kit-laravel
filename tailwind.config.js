218cf21bf466d167add202bb51af1d34e0e17ada
John Champ <72492771+john-champ@users.noreply.github.com>
2024-07-04 11:54:03 +0300

    Deps updated + Tailkit 3.19

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 // Tailkit (Tailwind CSS v3 Configuration)
-import plugin from "tailwindcss/plugin";
 import defaultTheme from "tailwindcss/defaultTheme";
-import colors from "tailwindcss/colors";
 
 import aspectRatio from "@tailwindcss/aspect-ratio";
 import forms from "@tailwindcss/forms";
@@ -10,7 +8,7 @@ import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./resources/views/**/*.blade.php"],
-  darkMode: "class",
+  darkMode: "selector",
   theme: {
     extend: {
       fontFamily: {
@@ -39,53 +37,7 @@ export default {
       animation: {
         "spin-slow": "spin-slow 8s linear infinite",
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            a: {
-              textDecoration: "none",
-              "&:hover": {
-                opacity: ".75",
-              },
-            },
-            img: {
-              borderRadius: defaultTheme.borderRadius.lg,
-            },
-          },
-        },
-      },
     },
   },
-  plugins: [
-    aspectRatio,
-    forms,
-    typography,
-    plugin(function ({ addUtilities }) {
-      const utilFormSwitch = {
-        ".form-switch": {
-          border: "transparent",
-          "background-color": colors.gray[300],
-          "background-image":
-            "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e\")",
-          "background-position": "left center",
-          "background-repeat": "no-repeat",
-          "background-size": "contain !important",
-          "vertical-align": "top",
-          "&:checked": {
-            border: "transparent",
-            "background-color": "currentColor",
-            "background-image":
-              "url(\"data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='-4 -4 8 8'%3e%3ccircle r='3' fill='%23fff'/%3e%3c/svg%3e\")",
-            "background-position": "right center",
-          },
-          "&:disabled, &:disabled + label": {
-            opacity: ".5",
-            cursor: "not-allowed",
-          },
-        },
-      };
-
-      addUtilities(utilFormSwitch);
-    }),
-  ],
+  plugins: [aspectRatio, forms, typography],
 };
